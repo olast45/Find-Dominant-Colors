6e351562ca266648a3fec245a6f4c2d7083b896a
Aleksandra <aleksandra.stepien@4soft.co>
2024-04-14 17:01:16 +0200

    style: Edit the page layout

--- dominant-colors/homepage.py
+++ dominant-colors/homepage.py
@@ -1,7 +1,20 @@
 import streamlit as st
 from src.main import pipeline
 
-uploaded_file = st.file_uploader("Choose a file (.jpg, .webp, .jpeg)")
-if uploaded_file is not None:
-    pipeline(uploaded_file)
-    st.image('dominant_colors.png', caption='Dominant Colors', use_column_width=True)
+if __name__ == "__main__":
+
+    st.title("Display Dominant Colors")
+    st.markdown("""
+        <style>
+        [data-testid ="stHeading"] {
+        margin-top: -75px;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    st.sidebar.title("Upload a picture")
+    uploaded_file = st.sidebar.file_uploader("Choose a file", type=["jpg", "webp", "jpeg"])
+    if uploaded_file is not None:
+        st.image(uploaded_file, use_column_width=True)
+        pipeline(uploaded_file)
+        st.image('dominant_colors.png', caption='Dominant Colors', use_column_width=True)
